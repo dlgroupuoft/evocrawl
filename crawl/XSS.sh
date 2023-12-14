5ccd7107e0c0b73e2a974dae2919f5dc8dd3a5b4
Xiangyu <gxy981113@gmail.com>
2023-12-14 10:38:38 -0500

    start attacking mode

--- crawl/XSS.sh
+++ crawl/XSS.sh
@@ -12,7 +12,7 @@ if [ -z "$1" ]
     MARGIN=0.1
     BLIND=2
     TOKEN_MODE=0
-    MODE=0
+    MODE=3
     REPLAY=0
     DATA_FOLDER=$PWD/../data/$1/
     DATA_FOLDER_EV2=$PWD/../data/$1/ev_instance_2/
@@ -22,8 +22,8 @@ if [ -z "$1" ]
     mkdir -p ${DATA_FOLDER}sim_set
     mkdir -p ${DATA_FOLDER}ev_set
     mkdir -p ${DATA_FOLDER}coordinator
-    #APPNAME=$1 DATA_FOLDER=$DATA_FOLDER USER_MODE='a' DEBUG_PRINT=$DBG_PRNT ENABLE_KAFKA=$KAFKA MF=$MARGIN REPLAY=$REPLAY MODE=$MODE pm2 start simCrawler.sh --name aSC_$1 #--no-autorestart
-    #sleep 1m
+    APPNAME=$1 DATA_FOLDER=$DATA_FOLDER USER_MODE='a' DEBUG_PRINT=$DBG_PRNT ENABLE_KAFKA=$KAFKA MF=$MARGIN REPLAY=$REPLAY MODE=$MODE pm2 start simCrawler.sh --name aSC_$1 #--no-autorestart
+    sleep 1m
     APPNAME=$1 DATA_FOLDER=$DATA_FOLDER USER_MODE='a' DEBUG_PRINT=$DBG_PRNT ENABLE_KAFKA=$KAFKA MF=$MARGIN BLIND=$BLIND REPLAY=$REPLAY MODE=$MODE pm2 start evoCrawler.sh --name aEC2_$1 #--no-autorestart
     #APPNAME=$1 DATA_FOLDER=$DATA_FOLDER_EV2 USER_MODE='a' DEBUG_PRINT=$DBG_PRNT ENABLE_KAFKA=$KAFKA MF=$MARGIN BLIND=2 REPLAY=$REPLAY MODE=$MODE pm2 start evoCrawler.sh --name aEC2_$1
     #sleep 30m
