2183c77c3f5cbfe59cafd2a0cd8d97acde2a1f93
Xiangyu <gxy981113@gmail.com>
2024-01-25 12:18:44 -0500

    update running script for macOS

--- mac_simtest.sh
+++ mac_simtest.sh
@@ -7,6 +7,6 @@ if [ -z "$1" ]
         mkdir -p data/$1/sim_log
         mkdir -p data/$1/sim_set
         mkdir -p data/$1/sim_responses
-        DATA_FOLDER=$PWD/data/$1/ DEBUG_PRINT=1 BLIND=0 APPNAME=$1 USER_MODE='a' ENABLE_KAFKA=0 REPLAY=0 MODE=1 node node_modules/testcafe/bin/testcafe.js 'chrome' crawl/simplecrawler.js \
+        DATA_FOLDER=$PWD/data/$1/ DEBUG_PRINT=1 BLIND=0 APPNAME=$1 USER_MODE='a' ENABLE_KAFKA=0 REPLAY=0 MODE=1 testcafe 'chrome' crawl/simplecrawler.js \
         -e -u --disable-multiple-windows -q --ajax-request-timeout 4000 --selector-timeout 1000
 fi
