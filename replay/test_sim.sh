41097054e1ec2b5a692357940113db3100273375
Xiangyu <gxy981113@gmail.com>
2024-01-26 09:54:41 -0500

    daily updates

--- replay/test_sim.sh
+++ replay/test_sim.sh
@@ -1,6 +1,9 @@
+APPNAME=$1
 rm -r ../data/$APPNAME/b_nav_cus/
+rm -r ../data/$APPNAME/b_nav/
+mkdir ../data/$APPNAME/b_nav/
 mkdir ../data/$APPNAME/b_nav_cus/
 #mkdir ../data/$APPNAME-ev/sim_responses/
 #mkdir ../data/$APPNAME-ev/ev_responses/
-node ../node_modules/testcafe/bin/testcafe.js 'chrome' customized_replayer.js \
--e -u --disable-multiple-windows -q --ajax-request-timeout 4000 --selector-timeout 1500
+APPNAME=$1 node ../node_modules/testcafe/bin/testcafe.js 'chrome --headless' ../replay/replayer.js \
+-e -u --disable-multiple-windows -q --ajax-request-timeout 4000 --selector-timeout 200
