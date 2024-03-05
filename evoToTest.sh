090ac6c5884d9cada40821460047b7642bd1a058
Xiangyu <gxy981113@gmail.com>
2024-03-05 13:46:13 -0500

    change to headless mode for chrome

--- evoToTest.sh
+++ evoToTest.sh
@@ -8,6 +8,6 @@ if [ -z "$APPNAME" ]
         mkdir -p data/$APPNAME/ev_log
         mkdir -p data/$APPNAME/ev_set
         mkdir -p data/$APPNAME/ev_responses
-        DEBUG_PRINT=1 BLIND=2 APPNAME=$APPNAME USER_MODE='a' ENABLE_KAFKA=0 REPLAY=0 MODE=0 node node_modules/testcafe/bin/testcafe.js 'chrome' crawl/evolutionarycrawler.js \
+        DEBUG_PRINT=1 BLIND=2 APPNAME=$APPNAME USER_MODE='a' ENABLE_KAFKA=0 REPLAY=0 MODE=0 node node_modules/testcafe/bin/testcafe.js 'chrome --headless' crawl/evolutionarycrawler.js \
         -e -u --disable-multiple-windows -q --ajax-request-timeout 4000 --selector-timeout 100
 fi 
