02c94e51078901c85122e73c68d3502f8cd7e6d2
gxy1113 <gxy981113@gmail.com>
2024-06-28 15:31:46 -0400

    test

--- crawl/simCrawler.sh
+++ crawl/simCrawler.sh
@@ -1,2 +1,2 @@
 node ../node_modules/testcafe/bin/testcafe.js 'chrome --headless' simplecrawler.js \
--e -u --disable-multiple-windows -q --ajax-request-timeout 4000 --selector-timeout 200
+-e -u --disable-multiple-windows -q --ajax-request-timeout 4000 --selector-timeout 2000
